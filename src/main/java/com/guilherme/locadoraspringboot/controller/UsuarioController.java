c6bcb690dd73455588b6e03920394e5a57e879a5
Zeniker <gui.lls@hotmail.com>
2019-07-07 11:44:15 -0300

    Refatoração no login e logout.

--- src/main/java/com/guilherme/locadoraspringboot/controller/UsuarioController.java
+++ src/main/java/com/guilherme/locadoraspringboot/controller/UsuarioController.java
@@ -3,24 +3,16 @@ package com.guilherme.locadoraspringboot.controller;
 import com.guilherme.locadoraspringboot.dto.DefaultResponseDTO;
 import com.guilherme.locadoraspringboot.dto.usuario.CriacaoUsuarioRequestDTO;
 import com.guilherme.locadoraspringboot.dto.usuario.LoginRequestDTO;
+import com.guilherme.locadoraspringboot.service.LoginService;
 import com.guilherme.locadoraspringboot.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-
-import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
 
 @RestController
 @RequestMapping("/usuario")
@@ -29,34 +21,22 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @Resource
-    private AuthenticationManager authenticationManager;
+    @Autowired
+    private LoginService loginService;
 
     @RequestMapping(value = "/criar", method = RequestMethod.POST)
-    private DefaultResponseDTO criarUsuario(@Valid @RequestBody final CriacaoUsuarioRequestDTO criacaoUsuarioRequestDTO){
+    public DefaultResponseDTO criarUsuario(@Valid @RequestBody final CriacaoUsuarioRequestDTO criacaoUsuarioRequestDTO){
         return usuarioService.criarUsuario(criacaoUsuarioRequestDTO);
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void login(@Valid @RequestBody LoginRequestDTO loginRequestDTO, HttpServletRequest httpServletRequest){
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginRequestDTO.getEmail(),
-                loginRequestDTO.getSenha());
-
-        Authentication auth = authenticationManager.authenticate(token);
-
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(auth);
-        HttpSession session = httpServletRequest.getSession();
-        session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext);
+    public DefaultResponseDTO login(@Valid @RequestBody LoginRequestDTO loginRequestDTO, HttpServletRequest httpServletRequest){
+        return loginService.login(loginRequestDTO, httpServletRequest);
     }
 
     @RequestMapping(value = "/logout", method = RequestMethod.POST)
-    public void logout(HttpServletRequest httpServletRequest){
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.getAuthentication().setAuthenticated(false);
-
-        HttpSession session = httpServletRequest.getSession();
-        session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext);
+    public DefaultResponseDTO logout(HttpServletRequest httpServletRequest){
+        return loginService.logout(httpServletRequest);
     }
 
 }
---
+++ src/main/java/com/guilherme/locadoraspringboot/service/LoginService.java
@@ -0,0 +1,67 @@
+package com.guilherme.locadoraspringboot.service;
+
+import com.guilherme.locadoraspringboot.dto.DefaultResponseDTO;
+import com.guilherme.locadoraspringboot.dto.usuario.LoginRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY;
+
+@Service
+public class LoginService extends DefaultService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public DefaultResponseDTO login(LoginRequestDTO loginRequestDTO, HttpServletRequest httpServletRequest){
+        DefaultResponseDTO defaultResponseDTO = new DefaultResponseDTO();
+
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginRequestDTO.getEmail(),
+                    loginRequestDTO.getSenha());
+
+            Authentication auth = authenticationManager.authenticate(token);
+
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            securityContext.setAuthentication(auth);
+            HttpSession session = httpServletRequest.getSession();
+            session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext);
+
+            marcaRetornoOK(defaultResponseDTO);
+        } catch (BadCredentialsException e){
+            marcaErroNoRetorno(defaultResponseDTO, "Usuário ou senha incorretos");
+        }catch (Exception e){
+            marcaErroNoRetorno(defaultResponseDTO, e.getMessage());
+        }
+
+        return defaultResponseDTO;
+    }
+
+    public DefaultResponseDTO logout(HttpServletRequest httpServletRequest) {
+        DefaultResponseDTO defaultResponseDTO = new DefaultResponseDTO();
+
+        try{
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            securityContext.getAuthentication().setAuthenticated(false);
+
+            HttpSession session = httpServletRequest.getSession();
+            session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext);
+
+            marcaRetornoOK(defaultResponseDTO);
+        }catch (Exception e){
+            marcaErroNoRetorno(defaultResponseDTO, e.getMessage());
+        }
+
+        return defaultResponseDTO;
+    }
+
+}
